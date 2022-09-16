0d395e10d2338077d4f6429992a836d21ac44de5
Zakaria Fadli <zakaria.fadli@netatmo.com>
2022-09-16 20:28:23 +0200

    some keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,11 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<C-PageUp>",   ":bnext<CR>", opts)
+keymap("n", "<C-PageDown>", ":bprevious<CR>", opts)
+
+-- Clear highlighting
+keymap("n", "<leader><space>", ":noh<CR>", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
