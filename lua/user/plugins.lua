23befc02c537ea24ad77b783f800b2aa62fc6f25
Zakaria Fadli <zakaria.fadli@legrand.com>
2022-11-03 23:31:18 +0100

    plugins: Add markdow preview, only for markdown files

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -92,6 +92,9 @@ return packer.startup(function(use)
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
 
+  -- Markdown
+  use({ "iamcco/markdown-preview.nvim", run = function() vim.fn["mkdp#util#install"]() end, ft = 'markdown'})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
