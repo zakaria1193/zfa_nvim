fbda28f53eb24db5857e41e2861ec7efb53c46bd
Zakaria Fadli <zakaria.fadli@legrand.com>
2022-11-03 02:51:40 +0100

    keymaps: Clean up of existing key maps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,3 +1,4 @@
+-- If you DO want the mapping to be recursive, set the "remap" option to "true"
 local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
@@ -5,7 +6,7 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
---Remap space as leader key
+--Remap comma as leader key
 keymap("", "<,>", "<Nop>", opts)
 vim.g.maplocalleader = ","
 vim.g.mapleader = ","
@@ -18,36 +19,33 @@ vim.g.mapleader = ","
 --   term_mode = "t",
 --   command_mode = "c",
 
+------------------------------------------------------------------------------------------------
 -- Normal --
--- Better window navigation
--- keymap("n", "<C-h>", "<C-w>h", opts)
--- keymap("n", "<C-j>", "<C-w>j", opts)
--- keymap("n", "<C-k>", "<C-w>k", opts)
--- keymap("n", "<C-l>", "<C-w>l", opts)
-
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Warning: All <leader> based commands in normal mode should be added in whichkey.lua
+-- to make full use of whichkey
 
 -- Navigate buffers
 keymap("n", "<C-PageUp>",   ":bnext<CR>", opts)
 keymap("n", "<C-PageDown>", ":bprevious<CR>", opts)
 
 -- Clear highlighting
-keymap("n", "<leader><space>", ":noh<CR>", opts)
+keymap("n", "<leader><space>", ":noh<CR>", opts) -- TODO Move to which key
 
+-- Search for word under cursor on double click
+keymap("n", "<2-leftMouse>", "*", opts)
+
+
+------------------------------------------------------------------------------------------------
 -- Insert --
--- Press jk fast to exit insert mode 
-keymap("i", "jk", "<ESC>", opts)
 
+------------------------------------------------------------------------------------------------
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
--- When pasting over something, 
+-- When pasting over a selection in visual mode, replaced the _ registers with what was replace
+-- So you can keep replacing. FIXME Is this really the best way for this issue?
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
@@ -55,10 +53,12 @@ keymap("v", "p", '"_dP', opts)
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
+------------------------------------------------------------------------------------------------
 -- Terminal --
 -- Better terminal navigation
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- FIXME: Review this when started to use terminal mode
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
