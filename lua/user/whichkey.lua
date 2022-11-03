ddee11b4bec7504fcbcde7baf1b83f095c6ebb1a
Zakaria Fadli <zakaria.fadli@legrand.com>
2022-11-03 03:12:13 +0100

    whichkey: Switch buffer deletion map and saving

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -85,9 +85,9 @@ local mappings = {
     "Buffers",
   },
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-  ["w"] = { "<cmd>w!<CR>", "Save" },
+  ["s"] = { "<cmd>w!<CR>", "Save" },
   ["q"] = { "<cmd>q!<CR>", "Quit" },
-  ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
+  ["w"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
