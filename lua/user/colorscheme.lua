56eaa4a162cd254ed55c45c593fe69738893ef7a
Zakaria Fadli <zakaria.fadli@legrand.com>
2022-11-03 23:31:02 +0100

    colorscheme: Use pure lua fail safe laoding

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,8 @@
-vim.cmd [[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+local colorscheme = "darkplus"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  vim.cmd "colorscheme default"
+end
+
