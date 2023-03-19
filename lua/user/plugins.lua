ff71b2d31322288a497a3c7979c0ae0b24bc64f4
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-03-19 19:36:40 +0100

    Revert "nvim: Plugins: Update lsp plugs"
    This reverts commit d04dae08a7f980118ba079657ee3e26155493bb6.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -156,11 +156,10 @@ return packer.startup(function(use)
   -- })
 
 	-- LSP
-use({ "neovim/nvim-lspconfig"}) -- enable LSP
-use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
-use({ "hrsh7th/cmp-nvim-lsp" })
-use({ "jose-elias-alvarez/null-ls.nvim"}) -- for formatters and linters
-
+--	use({ "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" }) -- enable LSP
+--	use({ "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" }) -- simple to use language server installer
+--	use({ "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" }) -- for formatters and linters
+---	use({ "hrsh7th/cmp-nvim-lsp", commit = "main" })
 
 	-- Telescope
   use { "nvim-telescope/telescope.nvim" }
