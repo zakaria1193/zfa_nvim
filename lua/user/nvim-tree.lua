30c4c8653f9283385247de15056f4e5cd3958741
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-04-26 21:56:16 +0200

    nvim-tree: Close if last open buffer

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -67,3 +67,22 @@ nvim_tree.setup {
     },
   },
 }
+
+-- Autoclose nvim-tree when quitting nvim
+-- from https://github.com/nvim-tree/nvim-tree.lua/issues/1368
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local invalid_win = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(invalid_win, w)
+      end
+    end
+    if #invalid_win == #wins - 1 then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+    end
+  end
+})
