b760a7787e9935f314150cd8f01ab3d58d1eea38
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-05-13 02:47:58 +0200

    cmp: Cleanup bindings

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -68,30 +68,7 @@ cmp.setup {
     -- If select is true
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = false },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
+    ["<Tab>"] = cmp.config.disable
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
