cb5944d59bdceb91babb65b66f018755f7d43075
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-05-29 17:35:16 +0200

    lsp: C: Disable semantic highlighting for ifdef blocks

--- lua/user/lsp/settings/clangd.lua
+++ lua/user/lsp/settings/clangd.lua
@@ -2,3 +2,7 @@ local lspconfig = require("lspconfig")
 
 lspconfig.clangd.setup{}
 
+-- Disable semantic highlighting for #ifdef blocks
+-- see help lsp-semantic-highlighting
+-- This was found using Inspect
+vim.api.nvim_set_hl(0, '@lsp.type.comment.c', {sp = ''})
