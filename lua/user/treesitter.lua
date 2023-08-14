582cd94300392ca92d8b4847adfe79af3eea3b17
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-08-14 17:08:18 +0200

    nvim treesitter: Disable for large files

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,12 +3,23 @@ if not status_ok then
 	return
 end
 
+-- Declare variable for max lines
+local max_file_lines = 3000
+
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
+    is_supported = function ()
+      if vim.fn.strwidth(vim.fn.getline('.')) > max_file_lines
+        or vim.fn.getfsize(vim.fn.expand('%')) > 1024 * 1024 then
+        return false
+      else
+        return true
+      end
+    end
 	},
 	autopairs = {
 		enable = true,
@@ -18,7 +29,7 @@ configs.setup({
     enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = 5000, -- Do not enable for files with more than n lines, int
+    max_file_lines = max_file_lines, -- Do not enable for files with more than n lines, int
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   }
