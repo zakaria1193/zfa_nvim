dbecf44750bbc8e6eb10b91e052621172b5c014e
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-10-13 15:15:12 +0200

    null ls: Add more linters

--- lua/user/lsp/settings/null_ls.lua
+++ lua/user/lsp/settings/null_ls.lua
@@ -19,6 +19,13 @@ null_ls.setup({
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.markdownlint,
     null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.pylint.with({
+      diagnostics_postprocess = function(diagnostic)
+        diagnostic.code = diagnostic.message_id
+      end,
+    }),
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
