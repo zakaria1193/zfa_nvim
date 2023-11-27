e5d94b62a5605929f862ee3c4714e586ebb62ae4
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-11-27 23:51:24 +0100

    indent: Change colors to dimmer

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -11,13 +11,13 @@ local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#3f0d11" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#44310d" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#072a46" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#372311" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#223216" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#34103f" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#1e4c52" })
 end)
 
 require("ibl").setup {
