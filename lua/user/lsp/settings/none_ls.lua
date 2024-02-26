4d09a6c3166117ae7271a1f0ce49d3c443d60b18
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-02-26 14:32:15 +0100

    rename null ls to none ls

--- lua/user/lsp/settings/none_ls.lua
+++ lua/user/lsp/settings/none_ls.lua
@@ -18,7 +18,6 @@ null_ls.setup({
     null_ls.builtins.diagnostics.cppcheck,
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.pylint.with({
       diagnostics_postprocess = function(diagnostic)
