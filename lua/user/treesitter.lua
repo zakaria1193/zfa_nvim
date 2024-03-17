c991f76ad6fb485ce2b2b29da59625506b4c5c29
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-03-17 23:11:24 +0100

    Treesitter: Revamp config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -6,17 +6,38 @@ end
 -- Declare variable for max lines
 local max_file_lines = 3000
 
-configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
+-- Config for treesitter
+--
+-- Disable LSP error for missing fields "modules" that can't be used anyway
+---@diagnostic disable-next-line: missing-fields
+configs.setup {
+
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = "all",
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
     enable = true,
+
     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
+    --[[ disable = { "c", "rust" }, ]]
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
+    disable = function(_, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
@@ -29,8 +50,7 @@ configs.setup({
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
-	},
-
+  },
 	autopairs = {
 		enable = true,
 	},
@@ -43,7 +63,7 @@ configs.setup({
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   }
-})
+}
 
 require'treesitter-context'.setup{
   enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
