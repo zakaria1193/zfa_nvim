9daf5694c2901377d8f109fdaaa43d6ca1e00fd2
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-03-18 17:53:31 +0100

    Treesitter: Ignore COMMENT parser (very slow)

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -14,6 +14,10 @@ configs.setup {
 
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = "all",
+  -- List of parsers to ignore installing (or "all")
+  -- Warning /!\:
+  -- 'comment' is a very slow parser, treesitter highlighting will be very slow if it installed
+  ignore_install = { "comment" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -21,9 +25,6 @@ configs.setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
