7f55d26fd63bb71750499698503d452e331f6895
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-05-13 16:12:12 +0200

    folding: Move config to specific config file

---
+++ lua/user/folding.lua
@@ -0,0 +1,14 @@
+if vim.fn.has("nvim-0.10") == 1 then
+-- Treesitter folding 
+  vim.opt.foldmethod = "expr"
+  vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+  vim.opt.foldtext = ""
+  vim.opt.fillchars = "fold: "
+else
+  vim.opt.foldmethod = "indent"
+end
+
+-- Disable folding in Telescope results
+vim.cmd([[
+  autocmd FileType TelescopeResults setlocal nofoldenable
+]])
