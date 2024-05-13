8f458ddfedaf08fcb407681a5a798271b9bee035
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-05-13 14:53:05 +0200

    treesitter/vim folding: Link treesitter to folding

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -79,3 +79,8 @@ require'treesitter-context'.setup{
   zindex = 20, -- The Z-index of the context window
   on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 }
+
+
+-- Treesitter folding 
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
