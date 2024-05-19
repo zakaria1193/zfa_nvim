a1d73fca0f372f8dcfd20d0185980a3c300560fb
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-05-19 21:27:27 +0200

    plugins: Remove rust_tools (archived project) rely on LSP for niw

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -193,9 +193,6 @@ return packer.startup(function(use)
   -- Highlight CSS colors
   use({ "norcalli/nvim-colorizer.lua" })
 
-  -- Rust tools
-  use({ "simrat39/rust-tools.nvim" })
-
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
