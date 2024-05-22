d99399bfbed8f1b3f700fd8f0aac0762b2b12e91
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-05-22 15:56:14 +0200

    plugins: Add git blame plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -178,6 +178,7 @@ return packer.startup(function(use)
 
 	-- Git
   use({ "iberianpig/tig-explorer.vim" })
+  use({ 'f-person/git-blame.nvim' })
 
   -- Doxygen
   use({ "vim-scripts/DoxygenToolkit.vim" })
