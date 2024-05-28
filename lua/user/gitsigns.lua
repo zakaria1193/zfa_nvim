a3af76565e7d3be35b79959b0aff987a564557e3
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-05-28 17:36:33 +0200

    gitsigns: Change signs

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -5,8 +5,8 @@ end
 
 gitsigns.setup {
   signs = {
-    add          = { text = '│' },
-    change       = { text = '│' },
+    add          = { text = '+' },
+    change       = { text = '~' },
     delete       = { text = '_' },
     topdelete    = { text = '‾' },
     changedelete = { text = '~' },
