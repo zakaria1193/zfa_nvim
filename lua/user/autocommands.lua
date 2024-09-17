86fbe47833bdb4613ab87c866407cdd7971dde23
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-09-17 17:43:52 +0200

    markdown: Disable diagnosics by default

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -17,6 +17,7 @@ vim.cmd [[
     autocmd!
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal spell
+    autocmd FileType markdown lua vim.diagnostic.enable(false)
   augroup end
 
   augroup _auto_resize
